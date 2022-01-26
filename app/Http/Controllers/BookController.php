ff493e0ca175033f4c61793edc812f019635958f
git0usename <junk0add@gamil.com>
2022-01-26 12:01:25 +0900

    最短の返却日をまとめた配列をつくってコントロールに渡す

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -15,40 +15,40 @@ class BookController extends Controller
      */
     public function index()
     {
-        //booksテーブルの中身を変数に定義(コレクションのインスタンスとして取得される)
-        $books = Book::all(); 
-        $fromtoday = Bookonloan::where('returnDate','>=',date("Y-m-d"))->orderBy('bookId','asc')->orderBy('returnDate','asc')->get(); 
+        // //
+        // //booksテーブルの中身を変数に定義(コレクションのインスタンスとして取得される)
+        // $books = Book::all(); 
+        // $fromtoday = Bookonloan::where('returnDate','>=',date("Y-m-d"))->orderBy('bookId','asc')->orderBy('returnDate','asc')->get(); 
         
-        $bookid_arr= $fromtoday->pluck('bookId')->toArray();
-        // dd(gettype($fromtoday));
-        //bookId毎にカウント(得られる配列["bookid"=>"count数","bookid"=>"count数",...])
-        $onloan_number_arr = array_count_values($bookid_arr);
-        //カウントしたものと全冊数を比べる
-         //$onloan_number_arrのkey(bookid)を取得
-        $onloan_bookid_arr = array_keys($onloan_number_arr);
+        // $bookid_arr= $fromtoday->pluck('bookId')->toArray();
+        // // dd(gettype($fromtoday));
+        // //bookId毎にカウント(得られる配列["bookid"=>"count数","bookid"=>"count数",...])
+        // $onloan_number_arr = array_count_values($bookid_arr);
+        // //カウントしたものと全冊数を比べる
+        //  //$onloan_number_arrのkey(bookid)を取得
+        // $onloan_bookid_arr = array_keys($onloan_number_arr);
         
-        // $book_number_arr = []; //内容を格納するための空配列を用意
+        // // $book_number_arr = []; //内容を格納するための空配列を用意
 
-        $returnDate_min =[];        
+        // $returnDate_min_arr = [];        
 
-        //onloanテーブル(貸出中)の本について、booksテーブルからその本の冊数を取得
-        foreach($onloan_bookid_arr as $onloan_bookid){
-            $book_number = Book::where('id',$onloan_bookid)->value('number');
-            //貸出中の冊数と全冊数を比較
-            foreach($onloan_number_arr as $onloan_number){ //貸出中の冊数を取得
-                //もし全部借りられてたら、
-                if($book_number <= $onloan_number){
-                    $q=$fromtoday->unique('bookId');//filterして取り出す
-                    $returnDate_min = ["returnDate_min"=>$fromtoday->unique('bookId')];
-                    return $returnDate_min;
-                                      
-                }else{
-                    return "全数貸出中";
-                    
-                }
-            }
-            // array_push($book_number_arr,$book_number);
-        }
+        // //onloanテーブル(貸出中)の本について、booksテーブルからその本の冊数を取得
+        // foreach($onloan_bookid_arr as $onloan_bookid){
+        //     $book_number = Book::where('id',$onloan_bookid)->value('number');
+        //     //貸出中の冊数と全冊数を比較
+        //     foreach($onloan_number_arr as $onloan_number){ //貸出中の冊数を取得
+        //         //もし全部借りられてたら、
+        //         if($book_number <= $onloan_number){
+        //             $fromtoday_nodup = $fromtoday->unique('bookId'); //filterして取り出す
+        //             $fromtoday_nodup_search_arr = array_search($onloan_bookid, array_column($fromtoday_nodup, 'bookId')); //多次元配列内をbookIdで検索して、該当の内側配列を取り出す
+        //             $returnDate_min = ["returnDate_min" => $fromtoday_nodup_search_arr['returnDate']];
+        //             $returnDate_min_arr[$onloan_bookid] = $returnDate_min;                                      
+        //         }else{                   
+        //               //何もしない                  
+        //         }
+        //     }            
+        // }
+        // //
 
         $books_onloans = [];  //結合した内容を格納するための空配列を用意
         foreach ($books as $book){
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -20,38 +20,42 @@ class Book extends Model
         return $this->hasOne(BookOnloan::class,'bookId');
     }
 
-    //laravel p283
-    // public function minReturnday()
-    // {
-    //     $fromtoday = Bookonloan::where('returnDate','>=',date("Y-m-d"))->orderBy('bookId','asc')->orderBy('returnDate','asc')->get();
-    //     // dd($fromtoday);
-    //     $bookid_arr= $fromtoday->pluck('bookId')->toArray();
-    //     // dd(gettype($fromtoday));
-    //     //bookId毎にカウント(得られる配列["bookid"=>"count数","bookid"=>"count数",...])
-    //     $onloan_number_arr = array_count_values($bookid_arr);
-    //     //カウントしたものと全冊数を比べる
-    //      //$onloan_number_arrのkey(bookid)を取得
-    //     $onloan_bookid_arr = array_keys($onloan_number_arr);
+    // bookonloansテーブルから今日を含めた貸出中の本を抽出
+    //その結果から最短のreturnDateを取り出して、連想配列に格納する。["(bookIdの数字がkey)" => ["returnDate_min" => "(最短の返却日が入る)"]]
+    public function minReturnday()
+    {
         
-    //     // $book_number_arr = []; //内容を格納するための空配列を用意
+        //booksテーブルの中身を変数に定義(コレクションのインスタンスとして取得される)
+        $books = Book::all(); 
+        $fromtoday = Bookonloan::where('returnDate','>=',date("Y-m-d"))->orderBy('bookId','asc')->orderBy('returnDate','asc')->get(); 
+        
+        $bookid_arr= $fromtoday->pluck('bookId')->toArray();
+        // dd(gettype($fromtoday));
+        //bookId毎にカウント(得られる配列["bookid"=>"count数","bookid"=>"count数",...])
+        $onloan_number_arr = array_count_values($bookid_arr);
+        //カウントしたものと全冊数を比べる
+         //$onloan_number_arrのkey(bookid)を取得
+        $onloan_bookid_arr = array_keys($onloan_number_arr);
+        
+        // $book_number_arr = []; //内容を格納するための空配列を用意
 
-    //     //onloanテーブル(貸出中)の本について、booksテーブルからその本の冊数を取得
-    //     foreach($onloan_bookid_arr as $onloan_bookid){
-    //         $book_number = Book::where('id',$onloan_bookid)->value('number');
-    //         //貸出中の冊数と全冊数を比較
-    //         foreach($onloan_number_arr as $onloan_number){ //貸出中の冊数を取得
-    //             //もし全部借りられてたら、
-    //             if($book_number <= $onloan_number){
-    //                 $returnDate_min = ["returnDate_min"=>$fromtoday->unique('bookId')];
-    //                 return $returnDate_min;                    
-    //             }else{
-    //                 return "貸出可";
-    //                 // dd($returnDate_min) ;
-    //             }
-    //         }
-    //         // array_push($book_number_arr,$book_number);
-    //     }
+        $returnDate_min_arr = [];        
 
-    // }
-    
+        //onloanテーブル(貸出中)の本について、booksテーブルからその本の冊数を取得
+        foreach($onloan_bookid_arr as $onloan_bookid){
+            $book_number = Book::where('id',$onloan_bookid)->value('number');
+            //貸出中の冊数と全冊数を比較
+            foreach($onloan_number_arr as $onloan_number){ //貸出中の冊数を取得
+                //もし全部借りられてたら、
+                if($book_number <= $onloan_number){
+                    $fromtoday_nodup = $fromtoday->unique('bookId'); //filterして取り出す
+                    $fromtoday_nodup_search_arr = array_search($onloan_bookid, array_column($fromtoday_nodup, 'bookId')); //多次元配列内をbookIdで検索して、該当の内側配列を取り出す
+                    $returnDate_min = ["returnDate_min" => $fromtoday_nodup_search_arr['returnDate']];
+                    $returnDate_min_arr[$onloan_bookid] = $returnDate_min;                                      
+                }else{                   
+                      //何もしない                  
+                }
+            }            
+        }
+        return $returnDate_min_arr;    
 }
