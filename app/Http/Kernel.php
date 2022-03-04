42734da04a510a07935749d13bb89a192483fe56
git0usename <junk0add@gamil.com>
2022-03-04 14:59:49 +0900

    session

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            // 500エラー　session store not set on request 対策
+            \App\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -66,12 +68,12 @@ class Kernel extends HttpKernel
     ];
 
     /**
- * ミドルウェアの優先順位でソートされたリスト
- *
- * これにより、非グローバルミドルウェアは常に指定する順序で実行されます。
- *
- * @var string[]
- */
+     * ミドルウェアの優先順位でソートされたリスト
+     *
+     * これにより、非グローバルミドルウェアは常に指定する順序で実行されます。
+     *
+     * @var string[]
+     */
     protected $middlewarePriority = [
         \Illuminate\Cookie\Middleware\EncryptCookies::class,
         \Illuminate\Session\Middleware\StartSession::class,
@@ -82,5 +84,5 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \Illuminate\Auth\Middleware\Authorize::class,
-];
+    ];
 }
