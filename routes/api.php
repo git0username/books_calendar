28fa137cba14cd84985793c3f15d390d32c94de1
git0usename <junk0add@gamil.com>
2022-03-05 21:13:07 +0900

    1

--- routes/api.php
+++ routes/api.php
@@ -48,4 +48,6 @@ Route::group(["middleware" => ["auth:sanctum",'cache.headers:private;no_store;et
 // Laravel8から書き方が変更された
 // apiに対応したrestfulにしておく
 
+Route::group(['middleware' => ['web']], function () {
 Route::post('/login',[LoginController::class, 'authenticate']);
+});
