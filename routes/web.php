86fb002ca70ca31d2ce66bb560635b6946958c51
git0usename <junk0add@gamil.com>
2022-03-06 13:56:27 +0900

    1

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,11 @@ Route::middleware('cache.headers:private;no_store;etag')->group(function () {
     Route::get('/login',function () {
             return view('welcome'); //welcome.blade.php を返す
         })->name('login');//routeに名前を付ける middleware Authenticate.php 内のreturn先が route('login')になっているので、ログインページのパスに対してloginという名前を付けてあげると、認証漏れした時にログインページに飛ばされるようになる
-       
+        
+    Route::post('/login',[LoginController::class, 'authenticate']);   
 });
 
-Route::post('/login',[LoginController::class, 'authenticate']);
+// Route::post('/login',[LoginController::class, 'authenticate']);
 
 Route::get('/logout',  [LoginController::class, 'logout']);  
 
